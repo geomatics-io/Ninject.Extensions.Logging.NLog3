02827cf567519dc575be4ec5b82da47b4ffb933f
Christian Junk <christian.junk@amecfw.com>
2016-09-27 11:10:04 +0200

    Initial Commit.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿//-------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="bbv Software Services AG">
+//   Copyright (c) 2010 Software Services AG
+//   Remo Gloor (remo.gloor@gmail.com)
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+#region
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("Ninject NLog2 Integration Library")]
+[assembly: Guid("DB448757-1C51-4A3A-892D-AEDCADF812A4")]
+
+
+[assembly: AssemblyDescription("NLog2 Logging extension for Ninject")]
